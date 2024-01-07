7e5f3340906ae8077b0dc5eed365c9698423894e
chrzanekz <matix3010@gmail.com>
2024-01-07 22:07:30 +0100

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char imieNazwisko[] = "Jan Nowak";
+    int wiekUsera = 35;
+    printf("Hej, mam na imie %s oraz mam %d lat. \n", imieNazwisko, wiekUsera);
+    printf("That's fine, %s ", imieNazwisko);
+    
+    return 0;
+}
