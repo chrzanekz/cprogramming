afe9609d32fd836c73bd31f590e5d1c7f4a153dc
chrzanekz <matix3010@gmail.com>
2024-01-07 21:56:44 +0100

    Create home.c

---
+++ home.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf ("################################\n");
+    printf ("#            HOME              #\n");
+    printf ("#      ######      ######      #\n");
+    printf ("#      #    #      #    #      #\n");
+    printf ("#      #    #      #    #      #\n");
+    printf ("################################\n");
+    return 0;
+}
